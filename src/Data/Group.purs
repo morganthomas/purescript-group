dc41629be29ac6d4f5d140df3b3304e8c637a021
Morgan Thomas <morgan.a.s.thomas@gmail.com>
2016-11-05 23:37:31 -0600

    Add doc comments.

--- src/Data/Group.purs
+++ src/Data/Group.purs
@@ -4,9 +4,21 @@ import Data.Monoid (class Monoid)
 import Data.Ring (class Ring, negate)
 import Data.Monoid.Additive (Additive(..))
 
+-- | A `Group` is a `Monoid` with inverses. Instances
+-- | must satisfy the following law in addition to the monoid laws:
+-- |
+-- | ```text
+-- | forall x. ginverse x <> x = mempty = x <> ginverse x
+-- | ```
 class Monoid g <= Group g where
   ginverse :: g -> g
 
+-- | A `CommutativeGroup` is a `Monoid` with a commutative monoid operation.
+-- | Instances must satisfy the following law in addition to the group laws:
+-- |
+-- | ```text
+-- | forall x, y. x <> y = y <> x
+-- | ```
 class Group g <= CommutativeGroup g
 
 instance ringsAreAdditiveGroups :: (Ring r) => Group (Additive r) where
